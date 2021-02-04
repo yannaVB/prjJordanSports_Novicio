4f0e511c97f7f5d1671fed08231fc568ff913d3a
Anna <novicioannamarie22@gmail.com>
2021-02-05 00:17:41 +0800

    initial commit

---
+++ prjJordanSports_Novicio/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblYs = New System.Windows.Forms.Label()
+        Me.lblWeekly = New System.Windows.Forms.Label()
+        Me.lblBw = New System.Windows.Forms.Label()
+        Me.txtYs = New System.Windows.Forms.TextBox()
+        Me.txtBw = New System.Windows.Forms.TextBox()
+        Me.txtW = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblYs
+        '
+        Me.lblYs.AutoSize = True
+        Me.lblYs.Font = New System.Drawing.Font("Lucida Bright", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYs.Location = New System.Drawing.Point(6, 16)
+        Me.lblYs.Name = "lblYs"
+        Me.lblYs.Size = New System.Drawing.Size(94, 15)
+        Me.lblYs.TabIndex = 0
+        Me.lblYs.Text = "1 Year Salary:"
+        '
+        'lblWeekly
+        '
+        Me.lblWeekly.AutoSize = True
+        Me.lblWeekly.Font = New System.Drawing.Font("Lucida Bright", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeekly.Location = New System.Drawing.Point(41, 45)
+        Me.lblWeekly.Name = "lblWeekly"
+        Me.lblWeekly.Size = New System.Drawing.Size(58, 15)
+        Me.lblWeekly.TabIndex = 1
+        Me.lblWeekly.Text = "Weekly:"
+        '
+        'lblBw
+        '
+        Me.lblBw.AutoSize = True
+        Me.lblBw.Font = New System.Drawing.Font("Lucida Bright", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBw.Location = New System.Drawing.Point(30, 79)
+        Me.lblBw.Name = "lblBw"
+        Me.lblBw.Size = New System.Drawing.Size(70, 15)
+        Me.lblBw.TabIndex = 2
+        Me.lblBw.Text = "BiWeekly:"
+        '
+        'txtYs
+        '
+        Me.txtYs.Location = New System.Drawing.Point(108, 11)
+        Me.txtYs.Name = "txtYs"
+        Me.txtYs.Size = New System.Drawing.Size(168, 20)
+        Me.txtYs.TabIndex = 3
+        '
+        'txtBw
+        '
+        Me.txtBw.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.txtBw.Location = New System.Drawing.Point(108, 77)
+        Me.txtBw.Name = "txtBw"
+        Me.txtBw.ReadOnly = True
+        Me.txtBw.Size = New System.Drawing.Size(168, 20)
+        Me.txtBw.TabIndex = 4
+        '
+        'txtW
+        '
+        Me.txtW.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.txtW.Location = New System.Drawing.Point(108, 43)
+        Me.txtW.Name = "txtW"
+        Me.txtW.ReadOnly = True
+        Me.txtW.Size = New System.Drawing.Size(168, 20)
+        Me.txtW.TabIndex = 5
+        '
+        'btnCalc
+        '
+        Me.btnCalc.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnCalc.Font = New System.Drawing.Font("Lucida Fax", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalc.Location = New System.Drawing.Point(9, 17)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(82, 32)
+        Me.btnCalc.TabIndex = 6
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnClear.Font = New System.Drawing.Font("Lucida Fax", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(8, 91)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(82, 32)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Cl&ear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lucida Bright", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(159, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(305, 27)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "JORDAN SPORTS STORE"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnClear)
+        Me.GroupBox2.Controls.Add(Me.btnCalc)
+        Me.GroupBox2.Controls.Add(Me.btnPrint)
+        Me.GroupBox2.Location = New System.Drawing.Point(323, 143)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(102, 172)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        '
+        'btnPrint
+        '
+        Me.btnPrint.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnPrint.Font = New System.Drawing.Font("Lucida Fax", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrint.Location = New System.Drawing.Point(8, 54)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(82, 32)
+        Me.btnPrint.TabIndex = 8
+        Me.btnPrint.Text = "&Print"
+        Me.btnPrint.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblYs)
+        Me.GroupBox1.Controls.Add(Me.lblWeekly)
+        Me.GroupBox1.Controls.Add(Me.lblBw)
+        Me.GroupBox1.Controls.Add(Me.txtYs)
+        Me.GroupBox1.Controls.Add(Me.txtBw)
+        Me.GroupBox1.Controls.Add(Me.txtW)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 154)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(287, 119)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.prjJordanSports_Novicio.My.Resources.Resources.jordan
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(132, 115)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.IndianRed
+        Me.btnExit.Font = New System.Drawing.Font("Lucida Fax", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(329, 274)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(82, 32)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Peru
+        Me.ClientSize = New System.Drawing.Size(472, 353)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Jordan Sports Store"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblYs As Label
+    Friend WithEvents lblWeekly As Label
+    Friend WithEvents lblBw As Label
+    Friend WithEvents txtYs As TextBox
+    Friend WithEvents txtBw As TextBox
+    Friend WithEvents txtW As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents PrintForm1 As PowerPacks.Printing.PrintForm
+End Class
---
+++ prjJordanSports_Novicio/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+    Private Sub BtnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim wholesalary As Decimal = Val(txtYs.Text)
+        Dim weeksalary As Decimal = wholesalary / 52
+        Dim biweeklysalary As Decimal = wholesalary / 26
+        txtW.Text = FormatCurrency(weeksalary, 2)
+        txtBw.Text = FormatCurrency(biweeklysalary, 2)
+
+
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtBw.Clear()
+        txtW.Clear()
+        txtYs.Clear()
+        txtYs.Focus()
+    End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        btnCalc.Hide()
+        btnClear.Hide()
+        btnPrint.Hide()
+        PrintForm1.Print()
+        btnCalc.Show()
+        btnClear.Show()
+        btnPrint.Show()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub lblWeekly_Click(sender As Object, e As EventArgs) Handles lblWeekly.Click
+
+    End Sub
+
+    Private Sub lblBw_Click(sender As Object, e As EventArgs) Handles lblBw.Click
+
+    End Sub
+
+    Private Sub txtYs_TextChanged(sender As Object, e As EventArgs) Handles txtYs.TextChanged
+
+    End Sub
+
+    Private Sub txtBw_TextChanged(sender As Object, e As EventArgs) Handles txtBw.TextChanged
+
+    End Sub
+
+    Private Sub txtW_TextChanged(sender As Object, e As EventArgs) Handles txtW.TextChanged
+
+    End Sub
+
+    Private Sub lblYs_Click(sender As Object, e As EventArgs) Handles lblYs.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
